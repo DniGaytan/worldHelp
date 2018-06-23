084356fbd15e1eea3b204502d922acb9ce60692d
DniGaytan <dnigaytan07@gmail.com>
2018-06-23 15:49:26 -0500

    Se agrego el modelo User_Extra al archivo Users:models.py

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+#Saves user extra information that the Model User can't
+class User_extra(models.Model):
+	user = models.ForeignKey(User, on_delete = models.CASCADE)
+	profile_picture = models.FileField()
