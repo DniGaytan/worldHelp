57f9d0b019b5c2137c552939f3ba836bad1a9b4d
DniGaytan <dnigaytan07@gmail.com>
2018-06-24 00:10:12 -0500

    Se modificaron los campos de forms.py y se cambiaron los tags de login.html

--- Users/forms.py
+++ Users/forms.py
@@ -2,8 +2,12 @@ from django import forms
 from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget = forms.PasswordInput(attrs={'class':'form-control'}))
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+        widgets = {
+        	'username': forms.TextInput(attrs={'class':'form-control'}),
+        	'email': forms.TextInput(attrs={'class':'form-control'}),
+        }
