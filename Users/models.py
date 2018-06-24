c85dbc5a40dcb604c2a47953a70a8f1e0ae35e66
DniGaytan <dnigaytan07@gmail.com>
2018-06-24 02:54:48 -0500

    Se modifico la relacion de User extra con la tabla User

--- Users/models.py
+++ Users/models.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import User
 
 #Saves user extra information that the Model User can't
 class User_extra(models.Model):
-	user = models.ForeignKey(User, on_delete = models.CASCADE)
+	user = models.OneToOneField(User, on_delete = models.CASCADE)
 	profile_picture = models.FileField()
