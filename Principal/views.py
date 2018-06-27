3588b0e1bb51d2c62e0b2bc0b64c4d336ec44b5f
DniGaytan <dnigaytan07@gmail.com>
2018-06-27 00:41:34 -0500

    Se añadio la funcion de mostrar los eventos por eje en el sitio Main.html, se modificaron views.py, mainStyles.css y Main.html ademas se elimino el search bar de Activities_Page.html

--- Principal/views.py
+++ Principal/views.py
@@ -9,7 +9,11 @@ def index(request):
     # return render cargara el archivo html
 
     context = {
-    	"eventos": Activity.objects.all()
+    	"eventos": Activity.objects.all(),
+    	"eventos_vida": Activity.objects.filter(activity_type = "vida")[:4],
+    	"eventos_innovacion": Activity.objects.filter(activity_type = "innovacion")[:4],
+    	"eventos_energia": Activity.objects.filter(activity_type = "energia")[:4],
+    	"eventos_social": Activity.objects.filter(activity_type = "social")[:4],
     }
 
-    return render(request, 'Principal/Main.html', context)
+    return render(request, 'Principal/Main.html', context = context)
